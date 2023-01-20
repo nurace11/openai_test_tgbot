bf04d9d875636a697631a1bc4494bf5634f1c57b
Qwww Rere <qwertylollel@gmail.com>
2023-01-20 20:20:45 +0600

    FSM checking for admin using user telegram id

--- create_bot.py
+++ create_bot.py
@@ -2,14 +2,14 @@ from aiogram import Bot, Dispatcher
 import configparser
 import openai
 from aiogram.contrib.fsm_storage.memory import MemoryStorage #Store data in RAM
- 
+
 config = configparser.ConfigParser()
 config.read('resources/application.ini')
 
 storage = MemoryStorage()
 
 openai.api_key = config['OPEN_AI']['KEY']
-ownerTelegramId = config['BOT']['OWNER_ID']
+ownerTelegramId = int(config['BOT']['OWNER_ID'])
 bot = Bot(token= config['BOT']['TOKEN'])
 dp = Dispatcher(bot, storage=storage)
 
--- main/adminHandler.py
+++ main/adminHandler.py
@@ -1,10 +1,8 @@
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram import types, Dispatcher
-from create_bot import dp, bot
+from create_bot import dp, ownerTelegramId
 from aiogram.dispatcher.filters import Text
-
-ID = None
 
 class FSMAdmin(StatesGroup):
     photo = State()
@@ -13,20 +11,10 @@ class FSMAdmin(StatesGroup):
     price = State()
 
 
-#checking for admin
-@dp.message_handler(commands=['moderator'], is_chat_admin=True)
-async def make_changes_command(message: types.Message):
-    global ID
-    ID = message.from_user.id
-    await bot.send_message(message.from_user.id, 'You are admin')
-    await message.delete()
-
-
-
 # start of fsm dialogue
 # @dp.message_handler(commands='load', state=None)
 async def cm_start(message: types.Message):
-    if message.from_user.id == ID:
+    if message.from_user.id == ownerTelegramId:
         await FSMAdmin.photo.set()
         await message.reply('Send photo')
 
@@ -34,7 +22,7 @@ async def cm_start(message: types.Message):
 # catch first answer and put it in the dictionary
 # @dp.message_handler(content_types=['photo'], state=FSMAdmin.photo)
 async def load_photo(message: types.Message, state: FSMContext):
-    if message.from_user.id == ID:
+    if message.from_user.id == ownerTelegramId:
         async with state.proxy() as data:
             data['photo'] = message.photo[0].file_id
         await FSMAdmin.next()
@@ -43,7 +31,7 @@ async def load_photo(message: types.Message, state: FSMContext):
 
 # @dp.message_handler(state=FSMAdmin.name)
 async def load_name(message: types.Message, state: FSMContext):
-    if message.from_user.id == ID:
+    if message.from_user.id == ownerTelegramId:
         async with state.proxy() as data:
             data['name'] = message.text
         await FSMAdmin.next()
@@ -52,7 +40,7 @@ async def load_name(message: types.Message, state: FSMContext):
 
 # @dp.message_handler(state=FSMAdmin.description)
 async def load_desc(message: types.Message, state: FSMContext):
-    if message.from_user.id == ID:
+    if message.from_user.id == ownerTelegramId:
         async with state.proxy() as data:
             data['description'] = message.text
         await FSMAdmin.next()
@@ -61,7 +49,7 @@ async def load_desc(message: types.Message, state: FSMContext):
 
 # @dp.message_handler(state=FSMAdmin.price)
 async def load_price(message: types.Message, state: FSMContext):
-    if message.from_user.id == ID:
+    if message.from_user.id == ownerTelegramId:
         async with state.proxy() as data:
             data['price'] = message.text
         await message.reply('thank you')
@@ -92,9 +80,6 @@ def register_handlers_admin(dp: Dispatcher):
 
     dp.register_message_handler(cancel_adding_product_handler, state='*', commands='cancel')
     dp.register_message_handler(cancel_adding_product_handler, Text(equals='cancel', ignore_case=True),state='*')
-
-    dp.register_message_handler(make_changes_command, commands=['moderator'], is_chat_admin=True)
-
 
 
 
