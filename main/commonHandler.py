c99be2b8c538048161c0bc88a0bee51c1b95bedf
Qwww Rere <qwertylollel@gmail.com>
2023-01-21 02:15:35 +0600

    add auto text translate with google.
    pip install googletrans==3.1.0a0

--- main/commonHandler.py
+++ main/commonHandler.py
@@ -2,6 +2,25 @@ from aiogram import types, Dispatcher
 from datetime import datetime
 from create_bot import usersMessageDictionary, bot, dp, ownerTelegramId
 import openai
+from googletrans import Translator
+from googletrans import Translator
+
+# translator = Translator()
+# print(translator.detect('Я последняя буква в алфавите'))
+#
+# text = "Я не жалуюсь"
+# detected = translator.detect(text)
+# print(detected.lang)
+#
+# textToSend = text
+# if detected.lang != 'en':
+#     textToSend = translator.translate(text).text
+#     print(translator.translate(text).text)
+#
+# print(textToSend)
+# print(type(translator.translate("Как твои дела")))
+
+translator = Translator()
 
 
 # @dp.message_handler()
@@ -18,20 +37,36 @@ async def echo_message(message: types.Message):
         usersMessageDictionary.clear()
 
     else:
+        lang = translator.detect(message.text).lang
+
+        if lang != 'en':
+            message_to_send = translator.translate(message.text).text
+            print("GOOGLE TRANSLATED:", message_to_send)
+        else:
+            message_to_send = message.text
+
         if message.chat.id in usersMessageDictionary:
             usersMessageDictionary[message.chat.id] = usersMessageDictionary[message.chat.id] + "\nYou: {}".format(
-                message.text) + "\nFriend:"
+                message_to_send) + "\nFriend:"
             print('Ok1')
         else:
             usersMessageDictionary[message.chat.id] = "You: {}".format(
-                message.text) + "\nFriend:"
+                message_to_send) + "\nFriend:"
             print('Ok2')
 
         try:
             gpt_answer = open_ai_response(usersMessageDictionary[message.chat.id])
-            await bot.send_message(message.chat.id, gpt_answer)
-            usersMessageDictionary[message.chat.id] = usersMessageDictionary[message.chat.id] + gpt_answer
-            print(usersMessageDictionary)
+            if lang != 'en':
+                translated_gpt_answer = translator.translate(gpt_answer, dest=lang).text
+                await bot.send_message(message.chat.id, translated_gpt_answer)
+                usersMessageDictionary[message.chat.id] = usersMessageDictionary[message.chat.id] + gpt_answer
+                print(usersMessageDictionary)
+            else:
+                await bot.send_message(message.chat.id, gpt_answer)
+                usersMessageDictionary[message.chat.id] = usersMessageDictionary[message.chat.id] + gpt_answer
+                print(usersMessageDictionary)
+
+
         except openai.OpenAIError as ae:
             await message.reply("[ERROR MESSAGE]\n\n" + ae.error)
 
