ba23bb56418404fd8479644833bc38078c964bfe
Qwww Rere <qwertylollel@gmail.com>
2023-01-23 03:13:53 +0600

    set my commands
    Took 27 minutes

--- bot_tg.py
+++ bot_tg.py
@@ -1,7 +1,7 @@
 from aiogram import executor
 
 import create_bot
-from create_bot import dp
+from create_bot import dp, bot, commands
 from database import sqlite_db
 from entity.TgUser import TgUser
 import openai
@@ -10,8 +10,12 @@ import openai
 async def on_startup(_): # add this method in the executor.start_polling(on_startup=on_startup)
     print("Bot is online")
     sqlite_db.sql_start()
+
+    print('Set my commands', await bot.set_my_commands(commands))
+
     for userEntry in create_bot.usersRepository.find_all():
         create_bot.usersInMemoryDatabase.add(TgUser(userEntry[0], openai.Completion(), "", userEntry[1], userEntry[2]))
+
 
 
 from main import adminHandler, clientHandler, commonHandler
--- create_bot.py
+++ create_bot.py
@@ -2,6 +2,7 @@ from aiogram import Bot, Dispatcher
 import configparser
 import openai
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.types.bot_command import BotCommand
 from repository import TgUserRepository
 from entity.TgUser import TgUser
 
@@ -15,6 +16,25 @@ admins_ids = config['BOT']['OWNER_ID'].split(',')
 bot = Bot(token= config['BOT']['TOKEN'])
 dp = Dispatcher(bot, storage=storage)
 
+commands = [
+    BotCommand('/help', 'get all commands'),
+    BotCommand('/clear', 'clears prompt. '),
+    BotCommand('/stats', 'statistics'),
+    BotCommand('/menu', 'db test')
+]
+print(commands)
+
+admin_commands = commands.copy()
+print(admin_commands)
+admin_commands.extend(
+    [
+        BotCommand('/database', 'sqlite tg_user table'),
+        BotCommand('/database_ram', 'list of users'),
+        BotCommand('/load', 'final state machine (FSM) test'),
+        BotCommand('/moderator', 'admin buttons test'),
+    ]
+)
+print(admin_commands)
 
 usersRepository = TgUserRepository
 usersInMemoryDatabase = set()
